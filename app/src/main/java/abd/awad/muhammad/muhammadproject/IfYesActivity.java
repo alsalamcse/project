c67d00e103f9653262d95943969c795807310f24
user <muhammed.awad@outlook.com>
2017-11-02 14:09:23 +0200

    project

---
+++ app/src/main/java/abd/awad/muhammad/muhammadproject/IfYesActivity.java
@@ -0,0 +1,13 @@
+package abd.awad.muhammad.muhammadproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IfYesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_if_yes);
+    }
+}
--- app/src/main/java/abd/awad/muhammad/muhammadproject/login.java
+++ app/src/main/java/abd/awad/muhammad/muhammadproject/login.java
@@ -1,7 +1,9 @@
 package abd.awad.muhammad.muhammadproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -9,7 +11,7 @@ import android.widget.TextView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class login extends AppCompatActivity
+public class login extends AppCompatActivity implements View.OnClickListener
 {
     private EditText etName;
     private EditText etAge;
@@ -37,5 +39,21 @@ public class login extends AppCompatActivity
         btnSearch = (Button) findViewById(R.id.btnSearch);
         auth = FirebaseAuth.getInstance();
         firebaseUser = auth.getCurrentUser();
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        if (btnYes==view)
+        {
+            Intent i = new Intent(this, IfYesActivity.class);
+            startActivity(i);
+        }
+
+        if (btnNo==view)
+        {
+            Intent i = new Intent(this,IfNoActivity.class);
+            startActivity(i);
+        }
     }
 }
