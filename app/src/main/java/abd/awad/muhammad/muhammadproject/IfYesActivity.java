f5b691128d0b942d4aa82a3d80d96d4ef3e5020f
user <muhammed.awad@outlook.com>
2017-11-05 08:54:30 +0200

    project

--- app/src/main/java/abd/awad/muhammad/muhammadproject/IfYesActivity.java
+++ app/src/main/java/abd/awad/muhammad/muhammadproject/IfYesActivity.java
@@ -3,11 +3,15 @@ package abd.awad.muhammad.muhammadproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class IfYesActivity extends AppCompatActivity {
+public class IfYesActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_if_yes);
+
+
     }
 }
